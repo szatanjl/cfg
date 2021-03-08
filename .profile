szatanjl/dotfiles

profile: Configure browser

.profile
@@ -8,5 +8,6 @@ export SHELL=/bin/zsh
 export PAGER=less
 export EDITOR=vim
 export VISUAL=vim
+export BROWSER=firefox
 
 export LESSHISTFILE=-

szatanjl/cfg

.profile
@@ -7,3 +7,4 @@ export SHELL=/bin/zsh
 export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
+export BROWSER=chromium